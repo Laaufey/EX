2110decb811a3cd76c34fbcf7aadaa861fe2e3e6
Laufey Hjaltested <laufeyhjaltested@Laufeys-MacBook-Pro.local>
2020-02-04 10:53:44 +0100

    bubbles

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ function start() {
     const bubbles = document.querySelectorAll("#wrapper>div");
     console.log(bubbles)
     bubbles.forEach(bubble => {
-        const x = Math.random()*100;
-        const y = Math.random()*100;
-
-        bubble.style.transform = "translate("+x+"vw, "+y+"vh)";
+        const x = Math.random() * 100;
+        const y = Math.random() * 100;
+        const hue = Math.random() * 360;
+        bubble.style.background = "hsl(" + hue + ", 70%, 80%)";
+        bubble.style.transform = "translate(" + x + "vw, " + y + "vh)";
     })
 }
